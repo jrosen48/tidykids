9f5434dee4fc050527e4682ab50ba932bdfa4524
Joshua Rosenberg <jmrosen48@gmail.com>
2020-09-07 15:13:17 -0400

    update

--- R/tidykids_dictionary.R
+++ R/tidykids_dictionary.R
@@ -1,4 +1,4 @@
-#' State-by-State Spending on Kids Dataset
+#' Data Dictionary for State-by-State Spending on Kids Dataset
 #'
 #' Data dictionary for the tidykids dataset, a dataset containing state-by-year data on spending on kids in the United States, 1997-2016. The data was made available by and is attributable to Julia Isaacs, Eleanor Lauderback, and Erica Greenberg at the Urban Institute under the under the ODC Attribution License (https://opendatacommons.org/licenses/by/1-0/).
 #'
