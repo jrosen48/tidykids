d7929abf6b1d52564a4055aa0c61b7130269ff1d
Joshua Rosenberg <jmrosenberg@utk.edu>
2024-02-14 13:49:41 -0500

    updates for new data

--- data-raw/tidykids.R
+++ data-raw/tidykids.R
@@ -1,6 +1,6 @@
-d <- purrr::map_df(1:69,  readxl::read_excel, path = here::here("data-raw", "State-by-State Spending on Kids_0.xlsx"))
+d <- purrr::map_df(1:69,  readxl::read_excel, path = here::here("data-raw", "State-by-State Spending on Kids.xlsx"))
 
-dict <- readxl::read_excel(path = here::here("data-raw", "State-by-State Spending on Kids Data Dictionary File_0.xlsx"))
+dict <- readxl::read_excel(path = here::here("data-raw", "State-by-State Spending on Kids Data Dictionary File_2.xlsx"))
 
 d$worksheet <- rep(dict$`Variable name`, each = 51)
 d$type_of_var <- rep(c(rep("raw", 51), rep("inf_adj", 51), rep("inf_adj_perchild", 51)), 23)
@@ -14,5 +14,7 @@ tidykids$worksheet <- purrr::map_chr(generic_name, ~.[[1]])
 tidykids <- tidyr::pivot_wider(tidykids, names_from = type_of_var, values_from = val)
 
 tidykids <- dplyr::rename(tidykids, variable = worksheet, year = key)
+
+readr::write_csv(dict, "data-raw/tidykids_data_dictionary.csv")
 
 usethis::use_data(tidykids, overwrite = TRUE)
